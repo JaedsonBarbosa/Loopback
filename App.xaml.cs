c07c9a13351bb76122c7bd000b8e4a44076313bd
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-11-23 11:20:29 -0300

    O aplicativo agora usa o manifesto padrão.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+using System.Windows;
 
 namespace Loopback
 {
@@ -7,5 +11,39 @@ namespace Loopback
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            if (!IsRunAsAdministrator())
+            {
+                var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase)
+                {
+                    // The following properties run the new process as administrator
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                // Start the new process
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch (Exception)
+                {
+                    // The user did not allow the application to run as administrator
+                    MessageBox.Show("Sorry, this application must be run as Administrator.");
+                }
+
+                // Shut down the current process
+                Application.Current.Shutdown();
+            }
+        }
+
+        private bool IsRunAsAdministrator()
+        {
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            return wp.IsInRole(WindowsBuiltInRole.Administrator);
+        }
     }
 }
